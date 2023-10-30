5103abed9fa376ddf579b1267aeac7d830e42bbe
VirtualPikachu <137836434+VirtualPikachu@users.noreply.github.com>
2023-10-30 17:48:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function getWeatherData(location)
+{
+  const apiKey = "2060afecd4e502524553ce16fe102ebb"
+  const city = location
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
+return fetch(url)
+  .then(response => response.json())
+  .then(data=> {
+    const weatherData = {
+      temperature: data.main.temp,
+      condition: data.weather[0].main,
+      location: data.name,
+    };
+    document.getElementById("Temperature").innerText = "Temperature: " + weatherData.temperature + " Degrees C";
+    document.getElementById("Condition").innerText = "Condition: " + weatherData.condition;
+    document.getElementById("Location").innerText = "Location: " + weatherData.location;
+  
+  });
+}
+
